9c9ad047a0ffecb00f45724693856ec1b255bbc4
Asia Lie <asia.lie@softwire.com>
2024-11-19 14:15:59 +0000

    Remove table usage

--- src/interfaces/TableProps.ts
+++
@@ -1,9 +0,0 @@
-import { StationDetails, SubStationLocation } from './StationDetails';
-
-interface TableProps {
-    data: StationDetails;
-    loc: SubStationLocation;
-    facilitiesList: string[]; // row in table
-  }
-
-export default TableProps;
